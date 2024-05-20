6a70289e70ce6cb21837dae2746512ecc32b6769
Dmitry Hatsayuk <dmitry.hatsayuk@gmail.com>
2024-05-21 02:29:46 +0700

    Modified

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,42 @@
 package org.example;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] prices = {13, 17, 19, 25, 25, 25, 25, 25, 25, 27, 30};
+
+        System.out.println("Для 31: " + countMore(prices, 31)); // 0
+        System.out.println("Для 26: " + countMore(prices, 26)); // 2
+        System.out.println("Для 25: " + countMore(prices, 25)); // 2
+        System.out.println("Для 20: " + countMore(prices, 20)); // 8
+    }
+
+    public static int countMore(int[] prices, int money) {
+        if (prices[0] > money) {
+            return prices.length; // все недоступны
+        }
+
+        if (prices[prices.length - 1] < money) {
+            return 0; // все доступны
+        }
+
+        int left = 0;
+        int right = prices.length - 1;
+        while (left < right) {
+            int middle = (left + right) / 2;
+
+            // Ваш код:
+            if ((prices[middle - 1] <= money) & (prices[middle] > money)) {
+                return prices.length - middle;
+            }
+            if (prices[middle] <= money) {
+                left = middle + 1;
+            }
+            if (prices[middle] > money) {
+                right = middle - 1;
+            }
+
+        }
+        return 0;
     }
 }
--- src/test/java/org/example/CountTest.java
+++ src/test/java/org/example/CountTest.java
@@ -1,16 +1,32 @@
 package org.example;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class MainTest {
-@BeforeEach
-public void setUp(){
-    int[] prices = {13, 17, 19, 25, 25, 25, 25, 25, 25, 27, 30};
-int [] money = 
-}
-    @org.junit.jupiter.api.Test
-    void countMore() {
-        assertEquals(expected, actual)    }
+class CountTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "31,0",
+            "26,2",
+            "25,2",
+            "20,8"})
+
+    public void validCountMore(int money, int expected) {
+        int[] prices = {13, 17, 19, 25, 25, 25, 25, 25, 25, 27, 30};
+        Assertions.assertEquals(expected, Main.countMore(prices, money));
+    }
+    @ParameterizedTest
+    @CsvSource(value = {
+            "44,15",
+            "44,2",
+            "44,100",
+            "44,8"})
+    public void invalidCountMore (int money, int expected){
+        int[] prices = {13, 17, 19, 25, 25, 25, 25, 25, 25, 27, 30};
+        Assertions.assertNotEquals(Main.countMore(prices, money), expected);
+    }
+
 }
