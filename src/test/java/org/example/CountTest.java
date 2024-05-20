5db8fd20147a878b81a30c573dd6132372400a5d
Dmitry Hatsayuk <dmitry.hatsayuk@gmail.com>
2024-05-21 02:06:30 +0700

    Initial commit

---
+++ src/test/java/org/example/CountTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+@BeforeEach
+public void setUp(){
+    int[] prices = {13, 17, 19, 25, 25, 25, 25, 25, 25, 27, 30};
+int [] money = 
+}
+    @org.junit.jupiter.api.Test
+    void countMore() {
+        assertEquals(expected, actual)    }
+}
